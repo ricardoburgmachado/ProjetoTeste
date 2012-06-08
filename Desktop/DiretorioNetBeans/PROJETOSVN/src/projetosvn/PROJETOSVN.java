a41ca7b0ffa3a0c18a0aad1fb0d59fbe893b504d
= <=>
2012-06-08 15:47:55 -0300

    sdfgdsafsd

---
+++ Desktop/DiretorioNetBeans/PROJETOSVN/src/projetosvn/PROJETOSVN.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetosvn;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class PROJETOSVN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
